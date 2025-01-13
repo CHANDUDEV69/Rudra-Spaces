8e0acd603b12ebf1f03c43feb2514f35f3c23eb7
CHANDUDEV69 <iamchandudeva@gmail.com>
2025-01-13 17:05:30 +0530

    major update

---
+++ Assets/Js/header.js
@@ -0,0 +1,62 @@
+class Header extends HTMLElement {
+     constructor() {
+     var compStatusIsReady = false;
+          super();
+
+          
+          const header = document.querySelector("header-component");
+          // header navigation loader
+          const HeaderNavigationLoader = () =>{
+               const navMenu = document.createElement("nav");
+               const navLinks = document.createElement("ul");
+               const navLinksArray = ["Home", "About Us", "Services", "Gallery", "Blog", "Contact Us"];
+               navLinksArray.forEach((entry)=>{
+                    const navListItem = document.createElement("li");
+                    const navLink = document.createElement("a");
+                    const trimmedUrl = entry.toLocaleLowerCase().replace(/\s/g, '');
+                    navLink.setAttribute("href",  trimmedUrl + "." +  "html");
+                    navLink.innerHTML = entry;
+                    navListItem.appendChild(navLink);
+                    navLinks.appendChild(navListItem);
+               })
+               navMenu.appendChild(navLinks);
+               header.appendChild(navMenu);
+               header.style.display = "flex";
+               header.style.width = 100 + "%";
+               header.style.alignItems = "center";
+               header.style.justifyContent = "space-between";
+               header.style.gap = "12px"
+          }
+
+
+          // fetch logo image
+          async function logoImageLoader(){
+               const logoWrappper = document.createElement('div');
+               logoWrappper.classList.add('logo-wrapper');
+               logoWrappper.classList.add("loading");
+               // const headerPreloader = document.querySelector(".loading");
+               // headerPreloader.style.background = "red";
+               const result = await fetch('../Assets/Images/logo.png');
+               if(result.status == 200){
+                    const logoImg = new Image();
+                    logoImg.src = '../Assets/Images/logo.png';
+                    logoImg.style.width = "100%";
+                    logoWrappper.style.width = "180px";
+                    logoWrappper.appendChild(logoImg);
+                    logoImg.setAttribute('id','logo-wrapper');
+                    logoImg.setAttribute("elementtiming",'logo');
+                    header.appendChild(logoWrappper);
+                         HeaderNavigationLoader();
+                    }
+               }
+     
+          
+          logoImageLoader();
+
+         
+     }
+}
+          customElements.define('header-component', Header);
+          document.querySelector('#header').innerHTML = `<header-component></header-component>`;
+
+
